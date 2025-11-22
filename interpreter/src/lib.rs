1d98227518ea1948a8ee6469be866125b7e0c553
Maxi Myoga <l.kupsch1407.new+github@gmail.com>
2025-11-22 22:13:53 +0100

    Added tests for interpreter

--- interpreter/src/lib.rs
+++ interpreter/src/lib.rs
@@ -26,7 +26,7 @@ impl Interpreter {
         }
     }
 
-    fn expression(expression: Expression) -> Value {
+    pub(crate) fn expression(expression: Expression) -> Value {
         match expression {
             Expression::Literal(literal) => Self::literal_expression(&literal),
             Expression::Binary {
@@ -58,4 +58,69 @@ impl Interpreter {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+
+    macro_rules! test_all_ops {
+        ($name:ident, $left:expr, $right:expr, $result:expr, $op:ident, $suffix:ident) => {
+            paste::paste! {
+                #[test]
+                fn [<$name _ $suffix>]() {
+                    let expression: Expression = Expression::Binary{
+                        left: Box::new($left),
+                        operator: Operator::$op,
+                        right: Box::new($right)
+                    };
+                    let result: Value = Interpreter::expression(expression);
+                    assert_eq!(result, $result);
+                }
+            }
+        };
+        ($name:ident, $left:expr, $right:expr, $add:expr, $sub:expr, $mul:expr, $div:expr) => {
+            test_all_ops!($name, $left, $right, $add, Add, addition);
+            test_all_ops!($name, $left, $right, $sub, Subtract, subtraction);
+            test_all_ops!($name, $left, $right, $mul, Multiply, multiplication);
+            test_all_ops!($name, $left, $right, $div, Divide, division);
+        };
+    }
+
+    test_all_ops!(
+        integer,
+        Expression::Literal(Literal::Integer(5)),
+        Expression::Literal(Literal::Integer(2)),
+        Value::Integer(7),
+        Value::Integer(3),
+        Value::Integer(10),
+        Value::Integer(2)
+    );
+
+    test_all_ops!(
+        float,
+        Expression::Literal(Literal::Float(5.0)),
+        Expression::Literal(Literal::Float(2.0)),
+        Value::Float(7.0),
+        Value::Float(3.0),
+        Value::Float(10.0),
+        Value::Float(2.5)
+    );
+
+    test_all_ops!(
+        mixed,
+        Expression::Literal(Literal::Integer(5)),
+        Expression::Literal(Literal::Float(2.0)),
+        Value::Float(7.0),
+        Value::Float(3.0),
+        Value::Float(10.0),
+        Value::Float(2.5)
+    );
+
+    test_all_ops!(
+        mixed_reverse,
+        Expression::Literal(Literal::Float(5.0)),
+        Expression::Literal(Literal::Integer(2)),
+        Value::Float(7.0),
+        Value::Float(3.0),
+        Value::Float(10.0),
+        Value::Float(2.5)
+    );
+}
