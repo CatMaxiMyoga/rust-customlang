2a521cad97b37bf3a15798f79e00c4e94ff59173
Maxi Myoga <l.kupsch1407.new+github@gmail.com>
2025-11-22 20:08:08 +0100

    Added interpreter logic

--- interpreter/src/lib.rs
+++ interpreter/src/lib.rs
@@ -1,14 +1,61 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+//! Contains the interpreter for the programming language
+
+pub mod types;
+
+use parser::types::{Expression, Literal, Operator, Program, Statement};
+use types::Value;
+
+/// The interpreter for the programming language.
+pub struct Interpreter;
+
+impl Interpreter {
+    /// Interprets the AST and executes the program.
+    pub fn run(program: Program) {
+        for statement in program.statements {
+            Self::statement(statement);
+        }
+    }
+
+    fn statement(statement: Statement) {
+        match statement {
+            Statement::Expression(expr) => {
+                // TEMP: prints the result of expression statements
+                let expression_result: Value = Self::expression(expr);
+                println!("{expression_result:?}");
+            }
+        }
+    }
+
+    fn expression(expression: Expression) -> Value {
+        match expression {
+            Expression::Literal(literal) => Self::literal_expression(&literal),
+            Expression::Binary {
+                left,
+                operator,
+                right,
+            } => Self::binary_expression(*left, &operator, *right),
+        }
+    }
+
+    const fn literal_expression(literal: &Literal) -> Value {
+        match literal {
+            Literal::Integer(value) => Value::Integer(*value),
+            Literal::Float(value) => Value::Float(*value),
+        }
+    }
+
+    fn binary_expression(left: Expression, operator: &Operator, right: Expression) -> Value {
+        let left: Value = Self::expression(left);
+        let right: Value = Self::expression(right);
+
+        match operator {
+            Operator::Add => left + right,
+            Operator::Subtract => left - right,
+            Operator::Multiply => left * right,
+            Operator::Divide => left / right,
+        }
+    }
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod tests {}
