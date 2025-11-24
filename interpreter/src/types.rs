5fee9a3ab9bc0406e95009585be43675b6033761
Maxi Myoga <l.kupsch1407.new+github@gmail.com>
2025-11-24 19:55:40 +0100

    Changed how operators between Value types are implemented.

--- interpreter/src/types.rs
+++ interpreter/src/types.rs
@@ -5,6 +5,13 @@ use std::ops::{Add, Div, Mul, Sub};
 /// Represents the environment mapping variable names to their values.
 pub type Environment = std::collections::HashMap<String, Option<Value>>;
 
+trait Operations {
+    fn add(&self, lsh: &Value, rhs: &Value) -> Result<Value, String>;
+    fn sub(&self, lsh: &Value, rhs: &Value) -> Result<Value, String>;
+    fn mul(&self, lsh: &Value, rhs: &Value) -> Result<Value, String>;
+    fn div(&self, lsh: &Value, rhs: &Value) -> Result<Value, String>;
+}
+
 /// Represents all types of values Expressions can return when evaluated.
 #[derive(Debug, Clone, PartialEq)]
 pub enum Value {
@@ -17,105 +24,190 @@ pub enum Value {
 }
 
 impl Value {
-    fn apply<I, F, S>(
-        &self,
-        other: &Self,
-        int_op: I,
-        float_op: F,
-        string_op: S,
-    ) -> Result<Self, String>
-    where
-        I: Fn(i64, i64) -> Result<Self, String>,
-        F: Fn(f64, f64) -> Result<Self, String>,
-        S: Fn(&str, &str) -> Result<Self, String>,
-    {
-        match (self, other) {
-            (Self::Integer(lhs), Self::Integer(rhs)) => int_op(*lhs, *rhs),
-            #[allow(clippy::cast_precision_loss)]
-            (Self::Integer(lhs), Self::Float(rhs)) => float_op(*lhs as f64, *rhs),
-            #[allow(clippy::cast_precision_loss)]
-            (Self::Float(lhs), Self::Integer(rhs)) => float_op(*lhs, *rhs as f64),
-            (Self::Float(lhs), Self::Float(rhs)) => float_op(*lhs, *rhs),
-            (Self::String(lhs), Self::String(rhs)) => string_op(lhs, rhs),
-            (Self::String(_), _) | (_, Self::String(_)) => Err(String::from(
-                "Cannot perform arithmetic operations on strings with other types",
-            )),
+    fn ops(&self) -> &dyn Operations {
+        match self {
+            Self::Integer(_) => &IntegerOperations,
+            Self::Float(_) => &FloatOperations,
+            Self::String(_) => &StringOperations,
         }
     }
 }
 
-macro_rules! impl_op {
-    ($trait:ident, $method:ident, $int_op:expr, $float_op:expr, $string_op:expr) => {
-        impl $trait<Value> for Value {
-            type Output = Result<Self, String>;
-            fn $method(self, other: Self) -> Result<Self, String> {
-                self.apply(&other, $int_op, $float_op, $string_op)
-            }
-        }
-
-        impl $trait<&Value> for Value {
-            type Output = Result<Self, String>;
-            fn $method(self, other: &Self) -> Result<Self, String> {
-                self.apply(other, $int_op, $float_op, $string_op)
-            }
-        }
-
-        impl $trait<Value> for &Value {
-            type Output = Result<Value, String>;
-            fn $method(self, other: Value) -> Result<Value, String> {
-                self.apply(&other, $int_op, $float_op, $string_op)
-            }
-        }
-
-        impl $trait<&Value> for &Value {
-            type Output = Result<Value, String>;
-            fn $method(self, other: &Value) -> Result<Value, String> {
-                self.apply(other, $int_op, $float_op, $string_op)
-            }
-        }
-    };
+impl Add for Value {
+    type Output = Result<Self, String>;
+    fn add(self, rhs: Self) -> Self::Output {
+        self.ops().add(&self, &rhs)
+    }
 }
 
-impl_op!(
-    Add,
-    add,
-    |lhs, rhs| Ok(Value::Integer(lhs + rhs)),
-    |lhs, rhs| Ok(Value::Float(lhs + rhs)),
-    |lhs, rhs| Ok(Value::String(lhs.to_owned() + rhs))
-);
+impl Sub for Value {
+    type Output = Result<Self, String>;
+    fn sub(self, rhs: Self) -> Self::Output {
+        self.ops().sub(&self, &rhs)
+    }
+}
 
-impl_op!(
-    Sub,
-    sub,
-    |lhs, rhs| Ok(Value::Integer(lhs - rhs)),
-    |lhs, rhs| Ok(Value::Float(lhs - rhs)),
-    |_, _| Err(String::from("Cannot perform subtraction on strings"))
-);
+impl Mul for Value {
+    type Output = Result<Self, String>;
+    fn mul(self, rhs: Self) -> Self::Output {
+        self.ops().mul(&self, &rhs)
+    }
+}
 
-impl_op!(
-    Mul,
-    mul,
-    |lhs, rhs| Ok(Value::Integer(lhs * rhs)),
-    |lhs, rhs| Ok(Value::Float(lhs * rhs)),
-    |_, _| Err(String::from("Cannot perform multiplication on strings"))
-);
+impl Div for Value {
+    type Output = Result<Self, String>;
+    fn div(self, rhs: Self) -> Self::Output {
+        self.ops().div(&self, &rhs)
+    }
+}
 
-impl_op!(
-    Div,
-    div,
-    |lhs, rhs| {
-        if rhs == 0 {
-            Err(String::from("Division by zero"))
-        } else {
-            Ok(Value::Integer(lhs / rhs))
+struct IntegerOperations;
+impl Operations for IntegerOperations {
+    fn add(&self, lhs: &Value, rhs: &Value) -> Result<Value, String> {
+        let Value::Integer(lhs) = lhs else {
+            unreachable!()
+        };
+        match rhs {
+            Value::Integer(rhs) => Ok(Value::Integer(lhs + rhs)),
+            #[allow(clippy::cast_precision_loss)]
+            Value::Float(rhs) => Ok(Value::Float(*lhs as f64 + rhs)),
+            Value::String(_) => Err("Cannot add Integer with non-numeric type".to_string()),
         }
-    },
-    |lhs, rhs| {
-        if rhs == 0.0 {
-            Err(String::from("Division by zero"))
-        } else {
-            Ok(Value::Float(lhs / rhs))
+    }
+
+    fn sub(&self, lhs: &Value, rhs: &Value) -> Result<Value, String> {
+        let Value::Integer(lhs) = lhs else {
+            unreachable!()
+        };
+        match rhs {
+            Value::Integer(rhs) => Ok(Value::Integer(lhs - rhs)),
+            #[allow(clippy::cast_precision_loss)]
+            Value::Float(rhs) => Ok(Value::Float(*lhs as f64 - rhs)),
+            Value::String(_) => Err("Cannot subtract Integer with non-numeric type".to_string()),
         }
-    },
-    |_, _| Err(String::from("Cannot perform division on strings"))
-);
+    }
+
+    fn mul(&self, lhs: &Value, rhs: &Value) -> Result<Value, String> {
+        let Value::Integer(lhs) = lhs else {
+            unreachable!()
+        };
+        match rhs {
+            Value::Integer(rhs) => Ok(Value::Integer(lhs * rhs)),
+            #[allow(clippy::cast_precision_loss)]
+            Value::Float(rhs) => Ok(Value::Float(*lhs as f64 * rhs)),
+            Value::String(_) => Err("Cannot multiply Integer with non-numeric type".to_string()),
+        }
+    }
+
+    fn div(&self, lhs: &Value, rhs: &Value) -> Result<Value, String> {
+        let Value::Integer(lhs) = lhs else {
+            unreachable!()
+        };
+        match rhs {
+            Value::Integer(rhs) => {
+                if *rhs == 0 {
+                    Err("Division by zero".to_string())
+                } else {
+                    Ok(Value::Integer(lhs / rhs))
+                }
+            }
+            Value::Float(rhs) => {
+                if *rhs == 0.0 {
+                    Err("Division by zero".to_string())
+                } else {
+                    #[allow(clippy::cast_precision_loss)]
+                    Ok(Value::Float(*lhs as f64 / rhs))
+                }
+            }
+            Value::String(_) => Err("Cannot divide Integer with non-numeric type".to_string()),
+        }
+    }
+}
+
+struct FloatOperations;
+impl Operations for FloatOperations {
+    fn add(&self, lhs: &Value, rhs: &Value) -> Result<Value, String> {
+        let Value::Float(lhs) = lhs else {
+            unreachable!()
+        };
+        match rhs {
+            #[allow(clippy::cast_precision_loss)]
+            Value::Integer(rhs) => Ok(Value::Float(lhs + *rhs as f64)),
+            Value::Float(rhs) => Ok(Value::Float(lhs + rhs)),
+            Value::String(_) => Err("Cannot add Float with non-numeric type".to_string()),
+        }
+    }
+
+    fn sub(&self, lhs: &Value, rhs: &Value) -> Result<Value, String> {
+        let Value::Float(lhs) = lhs else {
+            unreachable!()
+        };
+        match rhs {
+            #[allow(clippy::cast_precision_loss)]
+            Value::Integer(rhs) => Ok(Value::Float(lhs - *rhs as f64)),
+            Value::Float(rhs) => Ok(Value::Float(lhs - rhs)),
+            Value::String(_) => Err("Cannot subtract Float with non-numeric type".to_string()),
+        }
+    }
+
+    fn mul(&self, lhs: &Value, rhs: &Value) -> Result<Value, String> {
+        let Value::Float(lhs) = lhs else {
+            unreachable!()
+        };
+        match rhs {
+            #[allow(clippy::cast_precision_loss)]
+            Value::Integer(rhs) => Ok(Value::Float(lhs * *rhs as f64)),
+            Value::Float(rhs) => Ok(Value::Float(lhs * rhs)),
+            Value::String(_) => Err("Cannot multiply Float with non-numeric type".to_string()),
+        }
+    }
+
+    fn div(&self, lhs: &Value, rhs: &Value) -> Result<Value, String> {
+        let Value::Float(lhs) = lhs else {
+            unreachable!()
+        };
+        match rhs {
+            Value::Integer(rhs) => {
+                if *rhs == 0 {
+                    Err("Division by zero".to_string())
+                } else {
+                    #[allow(clippy::cast_precision_loss)]
+                    Ok(Value::Float(lhs / *rhs as f64))
+                }
+            }
+            Value::Float(rhs) => {
+                if *rhs == 0.0 {
+                    Err("Division by zero".to_string())
+                } else {
+                    Ok(Value::Float(lhs / rhs))
+                }
+            }
+            Value::String(_) => Err("Cannot divide Float with non-numeric type".to_string()),
+        }
+    }
+}
+
+struct StringOperations;
+impl Operations for StringOperations {
+    fn add(&self, lhs: &Value, rhs: &Value) -> Result<Value, String> {
+        let Value::String(lhs) = lhs else {
+            unreachable!()
+        };
+        match rhs {
+            Value::String(rhs) => Ok(Value::String(lhs.clone() + rhs)),
+            _ => Err("Cannot add String with non-String type".to_string()),
+        }
+    }
+
+    fn sub(&self, _lhs: &Value, _rhs: &Value) -> Result<Value, String> {
+        Err("Subtraction not supported for String type".to_string())
+    }
+
+    fn mul(&self, _lhs: &Value, _rhs: &Value) -> Result<Value, String> {
+        Err("Multiplication not supported for String type".to_string())
+    }
+
+    fn div(&self, _lhs: &Value, _rhs: &Value) -> Result<Value, String> {
+        Err("Division not supported for String type".to_string())
+    }
+}
